01413e1f33f359c9b0a29c2a1a9b1eec6c9345a6
Brice Nema <brice@briceamen.com>
2022-09-17 16:16:56 +0200

    Ajout de commandes basiques pour vérifier que le bot est bien connecté à Discord

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
-'use strict';
+// C'est le fichier server.js qui va nous permettre de faire fonctionner le bot
+// On importe les modules nécessaires
+const { Client, GatewayIntentBits } = require('discord.js');
+// On ajoute le ficher config.json qui contient les informations de connexion du bit (ID & Token)
+const { token } = require('./config.json');
 
-const express = require('express');
+// On crée un objet client qui va nous permettre de faire fonctionner le bot
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
-
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
+// Quand le serveur est prêt, on affiche un message dans la console
+client.once('ready', () => {
+	console.log('Ready!');
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+
+// On crée un évènement qui va nous permettre de faire fonctionner les commandes
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+  // On crée un objet qui contient les commandes
+	const { commandName } = interaction;
+  // On crée un objet qui contient les réponses
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply('Server info.');
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
+// On connecte le bot au serveur
+client.login(token);
